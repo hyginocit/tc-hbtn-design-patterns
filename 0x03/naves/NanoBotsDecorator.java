b83c80e5513ff833e686a1a90352022b9d68f28e
hyginocit <gnieves@ciandt.com>
2022-07-15 16:43:53 -0300

    0x03

---
+++ 0x03/naves/NanoBotsDecorator.java
@@ -0,0 +1,13 @@
+public class NanoBotsDecorator extends NaveEspacialDecorator{
+
+
+    public NanoBotsDecorator(NaveEspacial naveDecorada) {
+        super(naveDecorada);
+    }
+
+    @Override
+    public int getAtaque() {
+        return super.getAtaque() + 25;
+    }
+
+}
